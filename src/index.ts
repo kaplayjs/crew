e3494360e621ba8d9b0b2b4259acc5805c44cb56
lajbel <lajbelms@gmail.com>
2024-08-10 19:26:24 -0300

    feat: bag

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import bagSpriteO from "./assets/crew/bag-o.png";
+import bagSprite from "./assets/crew/bag.png";
 import beanSpriteO from "./assets/crew/bean-o.png";
 import beanSprite from "./assets/crew/bean.png";
 
@@ -6,7 +8,7 @@ export const assets = {
         "sprite": beanSprite,
         "outlined": beanSpriteO,
         "description": "A bean, one of much beans.",
-        "secret": "Some people thinks they hate Mark",
+        "secret": "Some people thinks she hates Mark",
         "author": "tga",
         "type": "crew",
         crewmeta: {
@@ -18,6 +20,25 @@ export const assets = {
             "hobbies": ["jumping", "bouncing", "rolling"],
             "favoriteFood": "rice",
             "favoriteColor": "green",
+        },
+    },
+    "bag": {
+        "sprite": bagSprite,
+        "outlined": bagSpriteO,
+        "description": "Bag is the one who carries the crew.",
+        "secret":
+            "He likes much eating grass, but more than that, he likes eating snow.",
+        "author": "tga",
+        "type": "crew",
+        crewmeta: {
+            "age": 7,
+            "height": 7,
+            "weight": 6300,
+            "species": "bag",
+            "origin": "Polygonal Savanna",
+            "hobbies": ["carrying", "eating", "sleeping"],
+            "favoriteFood": "grass",
+            "favoriteColor": "purple",
         },
     },
 };
