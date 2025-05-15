d1f437314d5670c6d5d8544bb7b28a55632798b3
lajbel <71136486+lajbel@users.noreply.github.com>
2025-05-15 15:02:09 -0300

    chore: more stuff

---
+++ assets/animals/karat.ts
@@ -0,0 +1,24 @@
+import karatSpriteO from "../../assets/animals/karat-o.png";
+import karatSprite from "../../assets/animals/karat.png";
+import type { SpriteCrewItem } from "../types/crew";
+
+export const karatData: SpriteCrewItem = {
+    name: "KaRat",
+    description: "A little Rat who lives in the rocks of the KAWorld.",
+    author: "lajbel",
+    secret: "Ryan's rat, a life companion",
+    category: "animals",
+    sprite: karatSprite,
+    outlined: karatSpriteO,
+    crewmeta: {
+        age: 0.5,
+        favoriteColor: "yellow",
+        favoriteFood: "cheese",
+        gender: 1,
+        height: 0.2,
+        hobbies: ["eating cheese", "exploring", "digging"],
+        origin: "KAWorld",
+        species: "rat",
+        weight: 0.5,
+    },
+};
---
+++ packs/KAWorld/Sprites/btfly/btfly.ts
@@ -0,0 +1,13 @@
+import type { SpriteCrewItem } from "@/types/crew";
+import btflySpriteO from "./btfly-o.png";
+import btflySprite from "./btfly.png";
+
+export const btflyData: SpriteCrewItem = {
+    name: "Butterfly",
+    description: "Beatiful butterfly.",
+    author: "tga",
+    secret: "Can't fly.",
+    category: "animals",
+    sprite: btflySprite,
+    outlined: btflySpriteO,
+};
