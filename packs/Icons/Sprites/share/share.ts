93cd768cea73c2baa9979690d0b4a85906ef60c8
lajbel <71136486+lajbel@users.noreply.github.com>
2025-05-15 23:21:17 -0300

    chore: add share icon

---
+++ packs/Icons/Sprites/share/share.ts
@@ -0,0 +1,14 @@
+import type { SpriteCrewItem } from "@/types/crew";
+import shareSpriteO from "./share-o.png";
+import shareSprite from "./share.png";
+
+export const shareData: SpriteCrewItem = {
+    kind: "Sprite",
+    name: "Share",
+    description: "The share icon.",
+    author: "lajbel",
+    secret: "What does this mean?",
+    tags: ["icons"],
+    sprite: shareSprite,
+    outlined: shareSpriteO,
+};
