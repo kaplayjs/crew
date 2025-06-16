7f9bfe063762939f4a6ae10a0f89a34747a869d3
imaginarny <gencurpeter@gmail.com>
2025-06-16 19:38:51 +0200

    feat: add menu icon

---
+++ packs/Icons/Sprites/menu/menu.ts
@@ -0,0 +1,16 @@
+import type { SpriteCrewItem } from "@/types/crew";
+import menuSpriteO from "./menu-o.png";
+import menuSprite from "./menu.png";
+
+export const menuData: SpriteCrewItem = {
+    kind: "Sprite",
+    name: "Menu",
+    description: "A menu icon.",
+    author: "lajbel",
+    secret: "Hamburger",
+    tags: ["ui", "icons"],
+    type: "UI",
+    origin: "Official",
+    sprite: menuSprite,
+    outlined: menuSpriteO,
+};
