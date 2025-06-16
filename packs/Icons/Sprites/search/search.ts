848c9ddcb28ab50ed0d3e58944fbfec87dab854e
imaginarny <gencurpeter@gmail.com>
2025-06-16 19:39:15 +0200

    feat: add search icon

---
+++ packs/Icons/Sprites/search/search.ts
@@ -0,0 +1,16 @@
+import type { SpriteCrewItem } from "@/types/crew";
+import searchSpriteO from "./search-o.png";
+import searchSprite from "./search.png";
+
+export const searchData: SpriteCrewItem = {
+    kind: "Sprite",
+    name: "Search",
+    description: "A magnifying glass.",
+    author: "lajbel",
+    secret: "Can't magnify",
+    tags: ["ui", "icons"],
+    type: "UI",
+    origin: "Official",
+    sprite: searchSprite,
+    outlined: searchSpriteO,
+};
