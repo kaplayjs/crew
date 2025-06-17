5a455b263268c75f5ed042bc5bd69bf897814c02
imaginarny <gencurpeter@gmail.com>
2025-06-17 21:05:21 +0200

    feat: add been emoji

---
+++ packs/Emojis/Sprites/been/been.ts
@@ -0,0 +1,16 @@
+import type { SpriteCrewItem } from "@/types/crew";
+import beenSpriteO from "./been-o.png";
+import beenSprite from "./been.png";
+
+export const beenData: SpriteCrewItem = {
+    kind: "Sprite",
+    name: "Been",
+    description: "Has been been a bean? Or bean't?..",
+    author: "misanthrope",
+    secret: "This is how beans look like when programming.",
+    tags: ["emojis"],
+    type: "Sprite",
+    origin: "Official",
+    sprite: beenSprite,
+    outlined: beenSpriteO,
+};
