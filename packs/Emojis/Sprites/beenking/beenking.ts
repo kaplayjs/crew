79063b6b5d377e8d626bb2c6435440f722a674d5
imaginarny <gencurpeter@gmail.com>
2025-06-17 21:05:40 +0200

    feat: add beenking gif

---
+++ packs/Emojis/Sprites/beenking/beenking.ts
@@ -0,0 +1,16 @@
+import type { SpriteCrewItem } from "@/types/crew";
+import beenkingSpriteO from "./beenking-o.gif";
+import beenkingSprite from "./beenking.gif";
+
+export const beenkingData: SpriteCrewItem = {
+    kind: "Sprite",
+    name: "Beenking",
+    description: "When been is thinking hard, is beenking.",
+    author: "misanthrope",
+    secret: "Hasn't been a secret here?",
+    tags: ["emojis"],
+    type: "Sprite",
+    origin: "Official",
+    sprite: beenkingSprite,
+    outlined: beenkingSpriteO,
+};
